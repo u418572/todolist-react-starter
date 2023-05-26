4c38ad78af1bb7cc375de3d2181b62a143a81c7e
u418572@kimo.com <u418572@kimo.com>
2023-05-26 12:06:12 +0800

    feat: setup router

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,18 @@
 import './App.scss';
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import { TodoPage, LoginPage, SignUpPage } from './pages';
 
 function App() {
   return (
     <div className="app">
-      <TodoPage />
+    <BrowserRouter>
+        <Routes>
+          <Route path="login" element={<LoginPage />} />
+          <Route path="signup" element={<SignUpPage />} />
+          <Route path="todo" element={<TodoPage />} />
+          <Route path="*" element={<HomePage />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
