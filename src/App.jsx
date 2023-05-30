f4b9114c491573a107ee48e18018393707295ef7
u418572@kimo.com <u418572@kimo.com>
2023-05-30 15:06:37 +0800

    feat add router basenmae

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,13 @@ import './App.scss';
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import { TodoPage, LoginPage, SignUpPage, HomePage } from './pages';
 import { AuthProvider } from './contexts/AuthContext'
+const basename= process.env.PUBLIC_URL
+
+
 function App() {
   return (
     <div className="app">
-    <BrowserRouter>
+    <BrowserRouter basename={basename}>
     <AuthProvider>
       <Routes>
           <Route path="login" element={<LoginPage />} />
